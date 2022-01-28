14028928a01ee7c87ab9a3dd5e3cfbde72a85244
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2022-01-28 11:37:05 +0530

    This program is related to Annotations

---
+++ SmartPhone.java
@@ -0,0 +1,13 @@
+package General.Jan_28;
+
+
+        import java.lang.annotation.Retention;
+        import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SmartPhone {
+
+
+    String os() default "Symbian";
+    int version() default 1;
+}
