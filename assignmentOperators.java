ef716492bf9b5d68f7100cfb3af69a39e6e4b860
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2022-01-31 19:42:38 +0530

    Add files via upload

---
+++ assignmentOperators.java
@@ -0,0 +1,20 @@
+package General;
+
+public class assignmentOperators {
+
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(x);  //4
+        x += 5;
+        System.out.println(x);  //9
+        x -= 5;
+        System.out.println(x);  //4
+        x *= 5;
+        System.out.println(x);  //20
+        x /= 4;
+        System.out.println(x);  //5
+        x %= 5;
+        System.out.println(x);  //0
+    }
+}
+
