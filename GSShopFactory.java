c0d1545a5673229d87cc7a23f1996da727757bee
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2022-02-08 15:03:03 +0530

    Add files via upload

---
+++ GSShopFactory.java
@@ -0,0 +1,12 @@
+package com.tns.application;
+import com.tns.framework.NormalAcc;
+import com.tns.framework.PrimeAcc;
+public class GSShopFactory {
+    public PrimeAcc getNewPrimeAccount(int accNo, String accNm, float charges) {
+        return null;
+    }
+    public NormalAcc getNewNormalAccount(int accNo, String accNm, float charges,float deliveryCharges){
+
+        return null;
+    }
+}
