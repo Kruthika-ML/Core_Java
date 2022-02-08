0142b4d2f2d732147e3ec60e6ea638a05afa382b
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2022-02-08 15:01:49 +0530

    Add files via upload

---
+++ ShopFactory.java
@@ -0,0 +1,14 @@
+package com.tns.framework;
+
+public class ShopFactory {
+    PrimeAcc getNewPrimeAcc(int AccNo,String accNm,float charges,boolean isPrime){
+
+        return null;
+    }
+
+    NormalAcc getNewNormalAccount(int AccNo, String accNm, float charges, float deliveryCharges) {
+        return null;
+    }
+
+}
+
